f9a7557c963400f377fb76fc81e541754210612b
Victor Ramirez <94077869+ramireezgui@users.noreply.github.com>
2022-05-12 21:57:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function tocaSom (idElementoAudio) {
+    const elemento = document.querySelector(idElementoAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play()
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
+}
+
+const ListadeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < ListadeTeclas.length; contador++){
+
+    const tecla = ListadeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        if(evento.code === "Enter" || evento.code === "Space") {
+        tecla.classList.add('ativa');
+        }
+      }
+      tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');}
+    
+}
